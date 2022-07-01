74ab0ad9412323b492323bb95004bfde2c721d80
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-07-01 12:42:43 -0400

    models and sever set up

--- models/index.js
+++ models/index.js
@@ -4,13 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category (category can have multiple products but a product can only belong to one category)
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag, allow products to have multiple tags and tags to have many products)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    // include its associated Category and Tag data
+      include: [
+        Category,
+        { 
+          model: Tag,
+          through: ProductTag,
+          as: 'tags'
+        }]
+    })
+      .then((productData) => res.status(200).json(productData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 // get one product
